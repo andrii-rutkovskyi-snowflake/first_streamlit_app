d3e8ddf08365df816b93c9b9cc0bf6752dc0340c
Andrii Rutkovskyi <109433850+andrii-rutkovskyi-snowflake@users.noreply.github.com>
2022-07-16 21:40:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breackfast Menu')
+streamlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Kale, Spinach & Rocket Smoothie')
+streamit.header('Hard-Boiled Free-Range Egg')
